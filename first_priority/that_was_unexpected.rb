df8296995846d0ad073a3e3f8cf1696c178f1447
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-22 17:18:14 +0000

    Automatically backed up by Learn

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -6,23 +6,23 @@ class Person
     @candy_lover = candy_lover
   end
 
-  def tall?
-    true
-  end
+#  def tall?
+#    true
+#  end
 
-  def basketball_player?
-    false
-  end
+#  def basketball_player?
+#    false
+#  end
 
-  def likes_candy?
-    true
-  end
+#  def likes_candy?
+#    true
+#  end
 
   def ballin_candy_lover?
     if @candy_lover == true
       "Aw sweet, this cat loves them some candy!"
     else
-      10.times do
+#      10.times do
         "not a ballin candy lover"
     end
   end
