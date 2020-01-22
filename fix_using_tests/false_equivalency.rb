82467fa4b519e465cf68c62bfb11066e823e3c35
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-22 17:26:46 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,9 +1,4 @@
 
-
-#def get_user_input
-#  gets.chomp.to_i
-#end
-
 def prompt_user
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
@@ -13,12 +8,6 @@ end
 def selection(num)
   array1 = ["YUM YUM MUNCH MUNCH MUNCH", "HAM HAM HAM IN MY TUMMY"]
   array1[num - 1]
-#  if num == 1
-#     "YUM YUM MUNCH MUNCH MUNCH"
-#  end
-#  if num == 2
-#     
-#  end
 end
 
 def runner
